3949ad760a28997c00657beff2d47f01e37141b3
tommyknocker81 <75935480+tommyknocker81@users.noreply.github.com>
2025-04-09 09:22:07 +0200

    Update search.js

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { from, to } = req.query;
 
   const propertyIds = [
-    // Replace with real Bentral property IDs
+    // Replace with your real Bentral property IDs
     "5f5451794d6a674d",
     "5f5451794d6a6b4d",
     "5f5451794d7a634d"
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
 
       for (const unit of units) {
         const unitId = unit.id;
+
         const detailsRes = await fetch(
           `https://api.bentral.com/v1/properties/${propertyId}/units/${unitId}?fields=availability&from=${from}&to=${to}`,
           { headers }
