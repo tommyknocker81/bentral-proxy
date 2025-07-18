8c16168c63ce5a191e6d95ea16c1dc468c14bfe5
tommyknocker81 <75935480+tommyknocker81@users.noreply.github.com>
2025-07-18 07:44:09 +0200

    Update search.js with new domain

--- api/search.js
+++ api/search.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { from, to, persons } = req.query;
 
-  res.setHeader("Access-Control-Allow-Origin", "https://beatrice-eb443d.webflow.io"); // 👈 ADD THIS
+  res.setHeader("Access-Control-Allow-Origin", "https://beatrice-eb443d-28e688c6164b9bfe20ae0ca.webflow.io"); // 👈 ADD THIS
   res.setHeader("Access-Control-Allow-Methods", "GET");
 
   const headers = {
